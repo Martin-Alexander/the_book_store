923c56f3e588a0c3948abd5b1656e80a3c822f7c
Martin-Alexander <martingianna@gmail.com>
2021-03-04 17:58:37 -0500

    routes for book stocks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  defaults format: :json do
+    namespace :api do
+      resources :books, only: [] do
+        resources :book_stocks, only: [:index, :create]
+      end
+
+      resources :book_stocks, only: [:update, :destroy]
+    end
+  end
 end
