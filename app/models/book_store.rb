a8f415d7c826ca57588c092dca61f0626b4391f5
Martin-Alexander <martingianna@gmail.com>
2021-03-04 23:21:18 -0500

    book store has many users and removes their book_store_id when deleted

--- app/models/book_store.rb
+++ app/models/book_store.rb
@@ -1,4 +1,13 @@
 class BookStore < ApplicationRecord
   has_many :book_stocks, dependent: :destroy
   has_many :books, through: :book_stocks
+  has_many :users
+
+  before_destroy :remove_foreign_key_from_users
+
+  private
+
+  def remove_foreign_key_from_users
+    users.update_all book_store_id: nil
+  end
 end
