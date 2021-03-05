a35cb36e50a640293f19d954c3a6e0f4a83d905b
Martin-Alexander <martingianna@gmail.com>
2021-03-04 23:20:39 -0500

    added users to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,14 @@ end
 puts "Cleaning database"
 Book.destroy_all
 BookStore.destroy_all
+User.destroy_all
 
 books = 20.times.map do
   puts "Creating book"
   Book.create! title: Faker::Book.title
 end
 
-5.times do
+5.times do |n|
   puts "Creating book store"
   book_store = BookStore.create! name: Faker::Company.name
 
@@ -24,5 +25,8 @@ end
     stock = rand(0..10)
     book_store.book_stocks.create! book: book, stock: stock, in_stock: stock.zero?
   end
+
+  puts "Creating user"
+  book_store.users.create! email: "user#{n}@email.com", password: "123456"
 end
 
