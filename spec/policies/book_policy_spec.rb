aaa010bbea0e14f1e83ffb4bb2ee410c428591ac
Martin-Alexander <martingianna@gmail.com>
2021-03-07 16:05:49 -0500

    wrong description for BookPolicy::Scope test

--- spec/policies/book_policy_spec.rb
+++ spec/policies/book_policy_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe BookPolicy, type: :policy do
     let(:user) { create(:user) }
     let(:book) { create(:book) }
 
-    it "only contains book stocks of your book store" do
+    it "returns all books" do
       expect(BookPolicy::Scope.new(user, Book).resolve).to include(book)
     end
   end
