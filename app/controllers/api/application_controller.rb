ad9099ec08787c4b8096f88034c079f0155382c2
Martin-Alexander <martingianna@gmail.com>
2021-03-06 19:40:12 -0500

    render 404 errors properly

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -3,11 +3,16 @@ module Api
     include Pundit
 
     rescue_from Pundit::NotAuthorizedError, with: :respond_with_forbidden
+    rescue_from ActiveRecord::RecordNotFound, with: :respond_with_not_found
 
     private
 
-    def respond_with_forbidden
-      render json: { message: "forbidden" }, status: :forbidden
+    def respond_with_forbidden(error)
+      render json: { message: error.message }, status: :forbidden
+    end
+
+    def respond_with_not_found(error)
+      render json: { message: error.message }, status: :not_found
     end
   end
 end
