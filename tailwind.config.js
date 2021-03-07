ce2f7871d43b08f224f3cf40a14e137a63d20218
Martin-Alexander <martingianna@gmail.com>
2021-03-07 17:58:24 -0500

    added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [
+    "./app/views/**/*.html",
+    "./app/views/**/*.html.erb",
+  ],
+  darkMode: false, // or "media" or "class"
+  theme: {
+    extend: {
+      spacing: {
+        '84': '21rem',
+        '96': '24rem',
+      }
+     }
+  },
+  variants: {
+
+  },
+  plugins: [],
+}
