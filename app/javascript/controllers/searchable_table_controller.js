eb7bfe9ae33c259fcf49e68c882c4cfb7bc4ba3b
Martin-Alexander <martingianna@gmail.com>
2021-03-08 03:22:45 -0500

    refactored SearchableTableController#filter

--- app/javascript/controllers/searchable_table_controller.js
+++ app/javascript/controllers/searchable_table_controller.js
@@ -6,33 +6,31 @@ export default class extends Controller {
   filter(event) {
     const search = event.currentTarget.value.toLowerCase();
 
-    const results = this.rowTargets.filter((row) => {
-      const seachableBy = row.dataset.searchableBy.toLowerCase();
-      const isAMatch = seachableBy.includes(search);
-
-      if (isAMatch) {
-        row.hidden = false;
-      } else {
-        row.hidden = true;
-      }
-
-      return isAMatch;
-    });
-
-    const numberOfHits = results.length;
-
-    if (search && numberOfHits === 0) {
-      // You searched and got no results
-      this.resultsSummaryTarget.innerText = `No results for "${search}"`;
-
-    } else if (search && numberOfHits > 0) {
-      // You searched and got some results
-      const wordForResults = numberOfHits === 1 ? "Result" : "Results";
-      this.resultsSummaryTarget.innerText = `${numberOfHits} ${wordForResults} for "${search}"`;
-
-    } else {
-      // There are no results because you didn't actually search
+    if (search === "") {
+      this.rowTargets.forEach(row => row.hidden = false);
       this.resultsSummaryTarget.innerText = "";
+    } else {
+      const results = this.rowTargets.filter((row) => {
+        const seachableBy = row.dataset.searchableBy.toLowerCase();
+        const isAMatch = seachableBy.includes(search);
+
+        if (isAMatch) {
+          row.hidden = false;
+        } else {
+          row.hidden = true;
+        }
+
+        return isAMatch;
+      });
+
+      const numberOfHits = results.length;
+
+      if (numberOfHits === 0) {
+        this.resultsSummaryTarget.innerText = `No results for "${search}"`;
+      } else if (numberOfHits > 0) {
+        const wordForResults = numberOfHits === 1 ? "Result" : "Results";
+        this.resultsSummaryTarget.innerText = `${numberOfHits} ${wordForResults} for "${search}"`;
+      }
     }
   }
 }
