ecd5572840a19a69cd4e7a3f22b35d100843a497
Martin-Alexander <martingianna@gmail.com>
2021-03-07 23:02:17 -0500

    clipboard copy refeactor

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,24 +1,28 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["message"];
+  static targets = ["message", "button"];
 
-  async copy(event) {
+  async copy() {
     const stringToBeCopied = this.element.dataset.clipboardCopy;
 
-    const button = event.currentTarget;
-    const originalTextOfButton = button.innerText;
+    // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Interact_with_the_clipboard#using_the_clipboard_api
 
     const permissionResult = await navigator.permissions.query({ name: "clipboard-write" });
 
     if (permissionResult.state == "granted" || permissionResult.state == "prompt") {
       await navigator.clipboard.writeText(stringToBeCopied)
 
-      button.innerText = "Copied!";
-
-      setTimeout(() => {
-        button.innerText = originalTextOfButton;
-      }, 1000);
+      this.flashMessage();
     }
+  }
+
+  flashMessage() {
+    const originalTextOfButton = this.buttonTarget.innerText;
+    this.buttonTarget.innerText = "Copied!";
+
+    setTimeout(() => {
+      this.buttonTarget.innerText = originalTextOfButton;
+    }, 1000);
   }
 }
